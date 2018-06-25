4001be161ee532a7eafb506ca2f76ea0fd6976c3
yui.honda <yui.a.honda@rakuten.com>
2018-06-25 04:16:40 +0000

    make it better

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,6 @@
+@if (count($errors) > 0)
+    @foreach ($errors->all() as $error)
+        <div class="alert alert-warning">{{ $error }}</div>
+    @endforeach
+    
+@endif
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('cover')
+    <div class="cover">
+        <div class="cover-inner">
+            <div class="cover-contents">
+                <h1>素敵なモノと出会う場所</h1>
+                <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+　　 @include('items.items')
+    {!! $items->render() !!}
+@endsection
